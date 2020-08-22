c8bf7aae4770016f6b41de0ea57c600a426f45e4
rashrayhan <rashrayhan@gmail.com>
2020-08-22 08:35:07 -0500

    spring rest api for multiple file upload

---
+++ src/main/java/com/example/fileupload/service/AmazonClient.java
@@ -0,0 +1,23 @@
+//package com.example.fileupload.service;
+//
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class AmazonClient {
+//
+//  private AmazonS3 s3client;
+//
+//  @Value("${amazonProperties.endpointUrl}")
+//  private String endpointUrl;
+//  @Value("${amazonProperties.bucketName}")
+//  private String bucketName;
+//  @Value("${amazonProperties.accessKey}")
+//  private String accessKey;
+//  @Value("${amazonProperties.secretKey}")
+//  private String secretKey;
+//  @PostConstruct
+//  private void initializeAmazon() {
+//    AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
+//    this.s3client = new AmazonS3Client(credentials);
+//  }
+//}
