963fd069e20f991f12c9818d611d989e158f1819
Mubarak Desh <mubbymubarak@gmail.com>
2020-08-31 09:44:59 -0400

    [1236] story changed

--- src/main/java/com/example/fileupload/controller/HomeController.java
+++ src/main/java/com/example/fileupload/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.example.fileupload.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +11,9 @@ public class HomeController {
     public String getHomePage() {
         return "This is the home page";
     }
-
+    
+    @PostMapping("/home/message")
+    public String postHomePage() {
+        return "message posted";
+    }
 }
